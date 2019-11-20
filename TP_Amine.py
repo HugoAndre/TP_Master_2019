d817dd633107ed10cb8136249e994c9ce61cceb4
yahaamine <57895668+yahaamine@users.noreply.github.com>
2019-11-20 11:09:39 +0100

    multiplication....

--- TP_Amine.py
+++ TP_Amine.py
@@ -6,6 +6,8 @@ Created on Mon Nov 18 11:48:16 2019
 """
 def verif():
     
+    
+    
     S=input('entrer le code de votre carte ?')
     X=len(S)
     
@@ -13,10 +15,41 @@ def verif():
         print ('code carte faux')
         return 0
     else:
-        print('bon nombre')
-        return 1
+        print('bon nombre de chiffre.....vérification des conditions de Luhn....')
+        return 1 
     
+    D=S[0]
+
+
+    if D==1:
+        D=2
+    elif D==2:
+            D=4
+    elif D==3:
+            D=6
+    elif D==4:
+            D=8
+    elif D==5:
+            D=1
+    elif D==6:
+            D=3
+    elif D==7:
+            D=5
+    elif D==8:
+            D=7
+    else:
+         D=9
+   
+    
+        
+
 Reponse=verif()
- #        for S in range (0,12):
+
+
+
+
+
+#        for S in range (0,12):
 #            S=S[] 
-    
+#    D=S[1][3][5][7][9][11]
+   
